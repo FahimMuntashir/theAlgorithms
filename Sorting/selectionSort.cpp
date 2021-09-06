921f2c1076fdb6b20ca3b68f9b7a592423b6bf96
FahimMuntashir <fahim.tech.me@gmail.com>
2021-09-07 01:58:04 +0600

    added ...

---
+++ Sorting/selectionSort.cpp
@@ -0,0 +1,67 @@
+
+/*
+*
+* Author : Fahim Muntashir
+* Handle: f12r
+*
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define f12r ios_base::sync_with_stdio(false), cin.tie(NULL)
+const double EPSILON = 1e-9;
+#define MOD 1000000007
+#define pi acos(-1)
+#define ll long long
+#define endl "\n"
+
+// selection Sorting
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        if (min != i)
+        {
+            swap(arr[i], arr[min]);
+        }
+    }
+}
+
+/********** Main()  function **********/
+
+int main()
+{
+    f12r;
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    selectionSort(arr, n);
+
+    // print
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
